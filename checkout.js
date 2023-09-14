d7cc24c2bb2dc9aa5afee93553d837b944b21e09
MrsGuzel <aleevgzll@gmail.com>
2023-09-14 16:44:24 +0200

    productList

--- checkout.js
+++ checkout.js
@@ -6,21 +6,62 @@ window.addEventListener("load", () => {
     // localStorage.setItem("taxRate", taxRate);
     // localStorage.setItem("shippingPrice", shippingPrice);
     // localStorage.setItem("FreeShippingPrice", FreeShippingPrice);
+    calculateCartPrice();
 });
 
 const navbarList = document.querySelector(".nav__list");
 const productList = document.querySelector("div.main__product-painel");
 
-navbarList.addEventListener("click", () => {
-    if(e.target.getAttribute("class")== "nav__list--btn"){
+navbarList.addEventListener("click", (e) => {
+    if(e.target.getAttribute("class") == "nav__list--btn" || "fa-regular fa-trash-can"){
         // e.target.parentElement.firstElementChild.innerText = "My Card";
         // e.target.previousElementSibling.innerText = "My Card";
         
-        productList.innerHTML ="No Product!"
+        productList.innerText ="No Product!"
         e.currentTarget.firstElementChild.innerText = "My Card";
-        // productList.childNodes.forEach(product => {
-        //     product.remove()
-        // })
+        calculateCartPrice();
+        
 
     }
+});
+
+productList.addEventListener("click", (e) => {
+    //minus
+    if(e.target.className == "fa-solid fa-minus"){
+        if( e.target.nextElementSibling.innerText > 1){
+        calculateProductPrice(e.target)
+        e.target.nextElementSibling.innerText --;
+        }else{
+            if(confirm(`${e.target.closes(".main__product-info".querySelector ("h2").innerText)} wil be removed`)){
+                e.target.closest(".main__product").remove()
+                calculateCartPrice()
+            }
+        }
+       
+    }
+    //plus
+    else if(e.target.classList.contains("fa-plus")){
+        e.target.previousElementSibling.innerText ++;
+        calculateProductPrice(e.target)
+    }
+    //remove
+    else if(e.target.id == "remove-product"){
+        if(confirm(`${e.target.closes(".main__product-info".querySelector ("h2").innerText)} wil be removed`)){
+            e.target.closest(".main__product").remove()
+            calculateCartPrice();
+        }
+    }
+    else{
+        alert("other element clicked")
+    }
+    
 })
+
+const calculateProductPrice = () => {
+    // product line calculations
+    
+}
+const calculateCartPrice = () => {
+    // cart total  calculations
+
+}
